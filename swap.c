1825a86700a73f451441efea87e4330aa6c587ac
susmithakota09 <85020518+susmithakota09@users.noreply.github.com>
2021-06-30 21:42:53 +0530

    swap.c

---
+++ swap.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void ssort(int arr[],int l)
+{
+    int i;
+
+    for(i=0;i<l;i++)
+    {
+        swap_max(arr,l,i);
+    }
+}
+
+void swap_max(int arr[],int l,int n)
+{
+    int j,k;
+    int temp;
+
+    for(j=k=n;j<l;j++)
+    {
+        if(arr[j] > arr[k])
+        {
+            k = j;
+        }
+    }
+
+    temp = arr[n];
+    arr[n] = arr[k];
+    arr[k] = temp;
+}
